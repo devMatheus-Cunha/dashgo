abe4af149de4ddba2f88dc1aaa3b89e0ce0b918a
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-12-06 20:09:06 -0300

    feat: applying theme to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { AppProps } from "next/app";
 
+// chakra
+import { ChakraProvider } from "@chakra-ui/react";
+
+// theme
+import { theme } from "../styles/theme";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
