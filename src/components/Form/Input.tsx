a1ca7dd5c3b99810de059e40bd71589b3f57cd59
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-12-07 17:43:14 -0300

    feat: create component Input

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+
+interface IInputProps extends ChakraInputProps{
+  name: string;
+  label?: string;
+}
+
+
+//--------------------------
+// Export
+//--------------------------
+export const Input = ({ label, name, ...rest }: IInputProps) => {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+};
