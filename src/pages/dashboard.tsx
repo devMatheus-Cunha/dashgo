ee1c191ee88b8d7bd5d148c5b18a9f2d80e0da50
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-12-08 16:34:07 -0300

    feat: applying component SideBat

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,14 +1,23 @@
+import React from "react";
+// chakra
+import { Flex } from "@chakra-ui/react";
+
 // components
-import Header from "../components/Header";
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 //--------------------------
 // Export
 //--------------------------
 const Dashboard: React.FC = () => {
   return (
-    <>
+    <Flex direction="column" h="100vh">
       <Header />
-    </>
+
+      <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 };
 
