5b7c35777ac52203d4d5886ee28fd6b84817b54d
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-12-09 18:48:19 -0300

    refactor: decomponetication of  SearchBox

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 
-const SearchBox: React.FC = () => {
+// chakra
+import { Flex, Input, Icon } from "@chakra-ui/react";
+
+// icons
+import { RiSearchLine } from "react-icons/ri";
+
+export const SearchBox: React.FC = () => {
 	return (
-		<h1>s</h1>
+		<Flex
+			as="label"
+			flex="1"
+			py="4"
+			px="8"
+			ml="6"
+			maxWidth={400}
+			alignSelf="center"
+			color="gray.200"
+			position="relative"
+			bg="gray.800"
+			borderRadius="full"
+		>
+			<Input
+				color="gray.50"
+				variant="unstyled"
+				px="4"
+				mr="4"
+				placeholder="Buscar na plataforma"
+				_placeholder={{ color: "gray.400" }}
+			/>
+			<Icon as={RiSearchLine} fontSize="20" />
+		</Flex>
+
 	);
 }
-
-export default SearchBox;
