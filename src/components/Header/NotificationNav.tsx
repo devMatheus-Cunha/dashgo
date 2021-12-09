783a14367157db58cc1f6ed8e9d2f98765b89b9b
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-12-09 18:48:28 -0300

    refactor: decomponetication of NotificationNav

--- src/components/Header/NotificationNav.tsx
+++ src/components/Header/NotificationNav.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 
-const NotificationNav: React.FC = () => {
+// chakra
+import { Flex, HStack, Icon } from "@chakra-ui/react";
+
+// icons
+import { RiNotificationLine, RiUserAddLine } from "react-icons/ri";
+
+export const NotificationNav: React.FC = () => {
 	return (
-		<h1>s</h1>
+		<Flex align="center" ml="auto">
+			<HStack
+				spacing="8"
+				mx="8"
+				pr="8"
+				py="1"
+				color="gray.300"
+				borderRadiusWidht={1}
+				borderColor="gray.700"
+			>
+				<Icon as={RiNotificationLine} fontSize="20" />
+				<Icon as={RiUserAddLine} fontSize="20" />
+			</HStack>
+		</Flex>
 	);
 }
-
-export default NotificationNav;
