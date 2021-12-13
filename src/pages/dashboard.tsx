c3d641fe3fbdc5a50b69e3dbd710f423c8be54b8
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-12-13 17:31:24 -0300

    fix: options values to Chart

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -18,12 +18,11 @@ import { options, series } from "../utils/ApexCharts";
 // apex //! for the import loading to happen only in the browser and not in the next node layer
 const Chart = dynamic(() => import("react-apexcharts"), {
 	ssr: false,
-})
+});
 
 //--------------------------
 // Export
 //--------------------------
-
 const Dashboard: React.FC = () => {
 	return (
 		<Flex direction="column" h="100vh">
@@ -37,13 +36,13 @@ const Dashboard: React.FC = () => {
 						<Text fontSize="lg" mb="4">
 							Inscritos da semana
 						</Text>
-						<Chart type="area" height={160} options={options} series={series} />
+						<Chart type="area" height={160} options={options as any} series={series} />
 					</Box>
 					<Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
 						<Text fontSize="lg" mb="4">
 							Taxa de abertura
 						</Text>
-						<Chart type="area" height={160} options={options} series={series} />
+						<Chart type="area" height={160} options={options as any} series={series} />
 					</Box>
 				</SimpleGrid>
 			</Flex>
